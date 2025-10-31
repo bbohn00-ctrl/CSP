a2b501e63ce3aabff94978bd7dbd4e5ced07a38b
bbohn00-ctrl <bbohn00@gmail.com>
2025-10-31 09:45:55 -0500

    Update 1.2.5.py

--- 1.2.5 - Shall We Play a Game/1.2.5.py
+++ 1.2.5 - Shall We Play a Game/1.2.5.py
@@ -4,7 +4,27 @@ import random as rand
 Screen = trtl.Screen()
 Screen.bgcolor("darkgreen")
 
+pen = trtl.Turtle()
+pen.hideturtle()
+pen.speed(10)
+pen.color("white")
 
+name = Screen.textinput("Would you like to play?")
+
+
+for x, label in [(start_x, "START"), (finish_x, "FINISH")]:
+    pen.penup()
+    pen.goto(x, -120)
+    pen.setheading(90)
+    pen.pendown()
+    pen.forward(240)
+    pen.penup()
+    pen.goto(x, 130)
+    pen.write(label, align="center", font=("Arial", 12, "bold"))
+
+deck = ((2,))
+
+screen.mainloop()
 #TODO
 #TODO
 #Green Felt UI
