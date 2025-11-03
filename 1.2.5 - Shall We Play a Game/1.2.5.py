bacf6cb3eaa80086650c89542050fb8deb417421
bbohn00-ctrl <bbohn00@gmail.com>
2025-11-03 11:09:55 -0600

    Update 1.2.5.py
    Commmit

--- 1.2.5 - Shall We Play a Game/1.2.5.py
+++ 1.2.5 - Shall We Play a Game/1.2.5.py
@@ -1,16 +1,24 @@
 import turtle as trtl
 import random as rand
 
+#Setup screen
 Screen = trtl.Screen()
 Screen.bgcolor("darkgreen")
 
 pen = trtl.Turtle()
 pen.hideturtle()
-pen.speed(10)
-pen.color("white")
+pen.speed(0)
+
+# Better way to make the deck without tedious uploads
+deck = [2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10, 11]* 4
 
 name = Screen.textinput("Would you like to play?")
-
+Credits = 5000
+#Cards slot cordinates
+player_start_X = -150
+player_start_Y = -150
+dealer_start_X = -150
+dealer_start_Y = 150
 
 for x, label in [(start_x, "START"), (finish_x, "FINISH")]:
     pen.penup()
