d4638529baa9042d94ee733b20fe623776cc9504
bbohn00-ctrl <bbohn00@gmail.com>
2025-11-06 10:11:57 -0600

    Update 1.2.5.py
    Made the code much more easy to read and changed the min bet to 500 instead of 1 which made more sense for my game. Also refined some of the user input parts to make a better product

--- 1.2.5 - Shall We Play a Game/1.2.5.py
+++ 1.2.5 - Shall We Play a Game/1.2.5.py
@@ -62,7 +62,7 @@ playing = True
 while playing:
     bet = None
     while bet is None:  # Repeats the question until a valid input is set in
-        bet = screen.numinput("Pick a Bet Amount", f"You have {credits}. Enter Your Bet:", minval=1, maxval=credits)
+        bet = screen.numinput("Pick a Bet Amount", f"You have {credits}. Enter Your Bet:", minval=500, maxval=credits)
         if bet is None:
             continue  
     bet = int(bet) 
@@ -93,40 +93,35 @@ while playing:
         dealer.append(rand.choice(deck))
         dealerscore_total = calc(dealer)
 
-#Show Results of the Hand
+#Show Results of the Hand/All possible outcomes
     pen.clear()
     draw_slots(-150, 0, "Player", player, playerscore_total)
     draw_slots(150, 0, "Dealer", dealer, dealerscore_total)
 
     if playerscore_total > 21:
         result = "Bust! Dealer Wins"
-        Chips -= bet
         credits -= bet
     elif dealerscore_total > 21 or playerscore_total > dealerscore_total:
         result = "You Win!"
-        Chips += bet
         credits += bet
     elif playerscore_total < dealerscore_total:
         result = "Dealer Wins"
-        Chips -= bet
         credits -= bet
     else:
         result = "Tie"
 
-    pen.goto(0. -180)
-    pen.write(f"{result}\nChips: {Chips}", align="center", font=("Arial", 16, "bold"))
     pen.goto(0, -180)
     pen.write(f"{result}\nCredits: {credits}", align="center", font=("Arial", 16, "bold"))
         
 #Check the players chip count-So the code knows when to end the game
     if credits >= 50000:
-        screen.textinput("Congradulations You Win!")
+        screen.textinput("Good","Congradulations You Win!")
         playing = False
     elif credits <= 0:
-        screen.textinput("Sorry your all out of luck")
+        screen.textinput("Sorry","Sorry your're all out of luck")
         playing= False
     else:
         again = screen.textinput("Would you like to play again?", "Type 'yes' or 'no':")
         if again is None or again.lower() != "yes":
             playing = False
-screen.bye()screen.mainloop()
+screen.mainloop()
